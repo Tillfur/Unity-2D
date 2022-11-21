6c3d27823660d964e5d1193abda696de2606977e
Tillfur <nemo0521nemo@gmail.com>
2022-11-21 10:58:18 +0800

    Update GameManager.cs
    新增分數

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -9,6 +9,8 @@ public class GameManager : MonoBehaviour
     GameObject car;
     GameObject flag;
     GameObject distance;
+    GameObject score;
+
 
     // Start is called before the first frame update
     void Start()
@@ -16,12 +18,18 @@ public class GameManager : MonoBehaviour
         this.car = GameObject.Find("car");
         this.flag = GameObject.Find("flag");
         this.distance = GameObject.Find("Distance");
+        this.distance = GameObject.Find("Distance");
+        this.score = GameObject.Find("Score");
+
     }
 
     // Update is called once per frame
     void Update()
     {
         float length = this.flag.transform.position.x - this.car.transform.position.x;
-        this.distance.GetComponent<Text>().text = "距離目標還有 " + length.ToString("F2") + "m";
+        this.distance.GetComponent<Text>().text = "距離目標還有: " + length.ToString("F2") + "m";
+
+        float ScoreNumber = 100 / length;
+        this.score.GetComponent<Text>().text = "分數" + ScoreNumber.ToString("F2");
     }
 }
